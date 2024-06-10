976c1904e4ac29cedc480c8e2fa78f0b3f70d1e2
JuanIngaCampos <juan_28_2003@hotmail.com>
2024-06-09 23:35:50 -0500

    xddd

--- views/administrador/administrador.php
+++ views/administrador/administrador.php
@@ -5,7 +5,7 @@
         <div class="col-sm-6 p-2">
             <h1 class="mb-3">Menú principal</h1>
 
-            <h1>xd</h1>
+            <h1>xdxx</h1>
         </div>
       </div>  
       <div class="row">
